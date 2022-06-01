9823a6e2ba4bb8b8c719c55d8ed89cdc17bcdbeb
sarae <sarae@DESKTOP-70R03FR>
2022-05-31 21:20:34 -0400

    Add project files.

---
+++ LeaveManagement.Web/Data/Employee.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace LeaveManagement.Web.Data
+{
+    public class Employee : IdentityUser
+    {
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public string TaxId { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public DateTime DateOfJoined { get; set; }
+
+    }
+}
+}
